fcefbd33f012257da59dacbfa8d50fac8c278097
raghveerdeol <deolraghveer5@gmail.com>
2024-06-14 12:31:42 +0200

    aggiunta api con i parametri

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 export default {
-data() {
-return {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+  data() {
+  return {
 
-}
-}
+  }
+  }
 }
 </script>
 
 <template>
-
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
 </template>
 
 <style scoped>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from 'axios';
+import { store } from '../store.js';
+
+export default {
+data() {
+return {
+    store,
+    apiFilmUrl: 'https://api.themoviedb.org/3/search/movie'
+}
+},
+methods: {
+    getFilmInfo(url, filmName){
+        axios.get(url, {
+            params: {
+                api_key: '83724394da4505a6dc047ce5485571d4',
+                query: 'ritorno al futuro',
+            }
+        })
+        .then(function (response) {
+            console.log(response.data.results);
+            this.store.filmInfo = response.data.results;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+        .finally(function () {
+            // always executed
+        });  
+            },
+},
+created() {
+    this.getFilmInfo(this.apiFilmUrl)
+}
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
