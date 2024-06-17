23bc3707f2c91a92cf442505dbf2f21cd422f4f3
raghveerdeol <deolraghveer5@gmail.com>
2024-06-17 12:24:43 +0200

    spostato mainSearch nel AppHeader

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,17 +1,67 @@
 <script>
+import axios from 'axios';
+import { store } from '../store.js';
+import MainSearch from './MainSearch.vue';
+
 export default {
+    components: {
+        MainSearch,
+    },
 data() {
 return {
-
+    store,
+    apiFilmUrl: 'https://api.themoviedb.org/3/search/movie',
+    apitvUrl: 'https://api.themoviedb.org/3/search/tv',
 }
-}
+},
+methods: {
+    getFilmInfo(url, filmName){
+        axios.get(url, {
+            params: {
+                api_key: '83724394da4505a6dc047ce5485571d4',
+                query: filmName,
+            }
+        })
+        .then(function (response) {
+            store.filmInfo = response.data.results;
+            console.log(response.data.results);
+        })
+        .catch(function (error) {
+        })
+        .finally(function () {
+            // always executed
+        });  
+    },
+    getTvsSeries(url, seriesName){
+        axios.get(url,{
+            params: {
+                api_key: '83724394da4505a6dc047ce5485571d4',
+                query: seriesName,
+            }
+        })
+        .then(function(response){
+            store.seriesInfo = response.data.results;
+            console.log(response.data.results);
+        })
+        .catch(function (error) {
+        })
+        .finally(function () {
+            // always executed
+        });  
+    },
+    movieName(title){
+        console.log(title)
+        this.getFilmInfo(this.apiFilmUrl, title)
+        this.getTvsSeries(this.apitvUrl, title)
+    }
+},
 }
 </script>
 
 <template>
-
+    <MainSearch @searched="movieName"/>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
